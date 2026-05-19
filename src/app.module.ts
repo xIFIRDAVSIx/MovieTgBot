40666665a7379b0a74c87171a8e3f87c4d5734ce
xIFIRDAVSIx <zarifboevfirdavs@gmail.com>
2026-05-19 15:55:41 +0500

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,20 @@ import { ConfigModule } from '@nestjs/config';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { BotUpdate } from './bot/bot.update';
 
+const token = process.env.TOKEN;
+
+if (!token) {
+  throw new Error('TOKEN is missing');
+}
+
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
+    ConfigModule.forRoot({ isGlobal: true }),
     TelegrafModule.forRoot({
-      token: process.env.BOT_TOKEN!,
+      token,
     })
   ],
   controllers: [AppController],
   providers: [AppService, BotUpdate],
 })
-export class AppModule {}
+export class AppModule { }
